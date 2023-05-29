3c7f1cfc417fc64bc371918aac04d5d4e303d9d0
Namountougou Dimanche <98207062+Dimanchecfa@users.noreply.github.com>
2023-05-29 10:10:05 +0000

    Delete commit.sh

--- commit.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch --env-filter 'if [ "$GIT_COMMITTER_EMAIL" = "kishansheth21@gmail.com" ]; then
-    export GIT_COMMITTER_NAME="Dimanchecfa"
-    export GIT_COMMITTER_EMAIL="dimanchenamo@gmail.com"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "kishansheth21@gmail.com" ]; then
-    export GIT_AUTHOR_NAME="Dimanchecfa"
-    export GIT_AUTHOR_EMAIL="dimanchenamo@gmail.com"
-fi' --tag-name-filter cat -- --branches --tags
